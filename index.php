3b32aa2d4b3d1be681bf390b34c363acf02eaf24
蓮池紘幸 <hiro@hasuikehiroshikounoMacBook-Pro.local>
2018-02-22 14:08:01 +0800

    index完成

--- index.php
+++ index.php
@@ -2,4 +2,16 @@
 	echo 'Hello world';
 	var_dump(1);
 
+	$name = 'Hiro';
+
  ?>
+
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title></title>
+ </head>
+ <body>
+ 	<h1>Hello Github!</h1>
+ </body>
+ </html>
